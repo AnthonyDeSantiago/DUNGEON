65cf09e2259febe86c04ca4b4950041b584fadfd
AnthonyDeSantiago <94334680+AnthonyDeSantiago@users.noreply.github.com>
2023-12-29 00:50:09 -0500

    Added Planning.gd

---
+++ planning.gd
@@ -0,0 +1,27 @@
+'''
+Matt:
+	Player:
+		combat
+		animation
+		sound effects
+	At least one level
+	At least one enemy:
+		Torch Guy:
+			combat
+			animation
+			sound effects
+	Music
+	
+
+Anthony:
+	Boss:
+		combat
+		animation
+		sound effects
+		phases
+	Boss Level
+	At least one level
+	At least one enemy:
+		Dynamite Guy
+		sound effects
+'''
